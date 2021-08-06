04d68d5e1d422243a653c2be21a403460bcf75f9
matholv512 <matholv512@outlook.com>
2021-08-06 23:25:04 +0000

    (06/08) Funções e início da busca sequencial

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,7 @@
+# Importar o valor da constrante PI
+# Math é o nome da biblioteca onde pi se encontra
+from math import pi
+
 # Função é um trecho de código que tem um nome e pode
 # receber informações externas para fazer seu trabalho.
 # Opcionalmente, uma função pode também produzir um valor
@@ -25,4 +29,29 @@ a = float(input('Informe a altura da pessoa: '))
 # Fazendo uma chamada à função imc()
 resultado = imc(p, a)
 
-print(f"I IMC calculado é {resultado}.")
+print(f"O IMC calculado é {resultado}.")
+
+def area_forma(base, altura, forma = "R"):
+    """
+        Função que calcula a área de uma das seguintes 
+        formas geométricas: retângulo, triângulo ou elipse
+        Parâmetro forma:
+        "R" == retângulo (parâmetro opcional com valor padrão)
+        "T" == triângulo
+        "E" == elipse
+    """
+    area = 0
+    if forma == "R": # Retângulo
+        area = base * altura
+    elif forma == "T": # Triângulo
+        area = base * altura / 2
+    elif forma == "E": # Elipse
+        area = (base / 2) * (altura / 2) * pi
+    return area
+
+print('----------------------------------------------')
+
+print(f"Retângulo 7.5x11: {area_forma(7.5, 11, 'R')}")
+print(f"Triângulo 8x12: {area_forma(8, 12, 'T')}")
+print(f"Círculo 15x15: {area_forma(15, 15, 'E')}")
+print(f"Quadrado 9.5x9.5: {area_forma(9.5, 9.5)}")
