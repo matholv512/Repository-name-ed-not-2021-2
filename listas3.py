e03962a474361ff8796ba4603790395103da57b2
matholv512 <matholv512@outlook.com>
2021-08-06 00:36:35 +0000

    (05/08) Listas e funções

--- listas3.py
+++ listas3.py
@@ -1,39 +1,38 @@
-frutas = ("laranja","maça","uva","pera","mamão","abacate","amora")
+# Lista de strings
+frutas = ["laranja", "maçã", "uva", "pêra", "mamão", "abacaxi", "amora"]
 
-# imprimindo apenas a fruta "uva"
+# Imprimindo apenas a fruta "uva"
 print(frutas[2])
 
-# substituindo "pera" por "melão"
+# Substituindo "pêra" por "melão"
 frutas[3] = "melão"
 print(frutas)
 
-# Descobrindo quantos elementos há na lista 
+# Descobrindo quantos elementos há na lista
+
 print(len(frutas))
 
-print('-------------------------------------------------')
+print('-----------------------------')
 
-# percorrendo e imprimindo cada um dos elementos da lista
+# Percorrendo e imprimindo cada um dos elementos da lista
 for fruta in frutas:
     print(fruta)
-
-print('-------------------------------------------------')
-
 
 # Percorrendo e imprimindo cada elemento e sua posição
 for i in range(len(frutas)):
     print(f"{frutas[i]} está na posição {i}")
 
-print('-------------------------------------------------')
+print('-----------------------------')
 
 # Percurso em ordem invertida
-# 1º argumento len(frutas - 1): a lista precisa começar no último elemento, que 
-# é determinado por len() - 1
-# 2º argumento: -1, porque o limite final não entra e precisamos terminar em 0
-# 3º argumento: -1, poruqe a lista precisa ser decrescente
-for j in range(len(frutas) - 1, -1, -1):
+# 1º argumento: len(frutas) - 1: a lista precisa começar no último elemento, que é
+# determinado por len() -1
+# 2 argumento: -1, porque o limite final não entra e precisamos terminar em 0
+# 3º argumento: -1, porque a lista precisa ser decrescente
+for j in range(len(frutas) -1, -1, -1):
     print(frutas[j])
 
-print('-------------------------------------------------')
+print('--------------------------')
 
 # Ordenando o vetor em ordem alfabética
 frutas.sort()
