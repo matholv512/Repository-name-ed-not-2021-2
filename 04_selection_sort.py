71d18b75a8b9d8301c4888d3dd8743b15296ec10
matholv512 <matholv512@outlook.com>
2021-08-27 23:33:03 +0000

    (27/08/2021) merge sort

--- 04_selection_sort.py
+++ 04_selection_sort.py
@@ -1,11 +1,11 @@
 # ALGORITMO DE ORDENAÇÃO SELECTION SORT
 #
-# Isola (seleciona) o primeiro elemento da lista e, em seguida, 
-# encontra o menor valor no restante da lista. Se o maior encontrado
+# Isola (seleciona) o primeiro elemento da lista e, em seguida,
+# encontra o menor valor no restante da lista. Se o valor encontrado
 # for menor que o valor selecionado, efetua a troca.
 # Em seguida, isola o segundo número da lista, buscando pelo menor
 # valor das posições subsequentes. Faz a troca entre os dois valores,
-# se necessário. Continua o processo, até isolar o penultimo elemento
+# se necessário. Continua o processo, até isolar o penúltimo elemento
 # da lista.
 
 comps = 0
@@ -16,6 +16,7 @@ def selection_sort(lista):
     """
         Função que implementa o algoritmo de ordenação selection sort
     """
+
     global comps, passadas, trocas
     comps = 0
     passadas = 0
@@ -25,12 +26,14 @@ def selection_sort(lista):
 
     # Seleciona (isola) o elemento que será comparado
     for pos_sel in range(len(lista) - 1):
-        passadas += 1 
+        passadas += 1
+
         pos_menor = pos_sel + 1
+
         # Rotina para encontrar o menor número no resto da lista
-        # Percurso da lista da posição i + 2 até o fim
+        # Percurso da lista da posição pos_sel + 2 até o fim
         for j in range(pos_sel + 2, len(lista)):
-            comps =+ 1
+            comps += 1
 
             # Se o elemento da posição atual (j) for menor
             # que o elemento na posição do menor (pos_menor),
@@ -45,33 +48,37 @@ def selection_sort(lista):
             lista[pos_menor], lista[pos_sel] = lista[pos_sel], lista[pos_menor]
             trocas += 1
 
-################################################################################
+#####################################################################
 
 # Valores das variáveis no início do selection sort:
 # pos_sel: 0 (onde está o 88)
 # pos_menor: 1 (onde está o 44)
 # j: 2 (onde está o 33)
-#nums = [88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
+# nums = [88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
 
-# NÂO é o pior caso: lista em ordem inversa
-nums = [99, 88, 77, 66, 55, 44, 33, 22, 11, 0]
+# NÃO é o pior caso: lista em ordem inversa
+# nums = [99, 88, 77, 66, 55, 44, 33, 22, 11, 0]
+
+# Melhor caso: lista já ordenada
+nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
 
 selection_sort(nums)
 print(nums)
 print(f"Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
 
-
-################################################################################
+#############################################################################
 
 from data.nomes_desord import nomes
 from time import time
-nomes_parcial = nomes[:100000]   # Usa apenas os primeiros 30 mil nomes
+
+#nomes_parcial = nomes[:30000]   # Usa apenas os primeiros 30 mil nomes
+
 ini = time()
-selection_sort(nomes_parcial)
+#selection_sort(nomes_parcial)
+selection_sort(nomes)
 fim = time()
 
-print(nomes_parcial)
+#print(nomes_parcial)
+print(nomes)
 print(f"Tempo: {fim - ini}")
 print(f"Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
-
-
--- 05_recursividade.py
+++ 05_recursividade.py
@@ -1,18 +1,17 @@
 # FATORIAL DE UM NÚMERO n
-# É igual ao número n multiplicado por todos os seus antecessores
-# antecedores até 1
+# É igual ao número n multiplicado por todos os seus
+# antecessores até 1
 #
-# 
 # 7! = 7 * 6 * 5 * 4 * 3 * 2 * 1 (5040)
 #
 # 5! = 5 * 4 * 3 * 2 * 1 (120)
 # 4! = 4 * 3 * 2 * 1 (24)
 # 3! = 3 * 2 * 1 (6)
 # 2! = 2 * 1 (2)
-# 1! = 1 
+# 1! = 1
 # 0! = 1 (por convenção)
 #
-# 5! = 5 * 4! 
+# 5! = 5 * 4!
 # 4! = 4 * 3!
 # 3! = 3 * 2!
 # 2! = 2 * 1!
@@ -31,23 +30,26 @@ def fatorial(n):
     return resultado
 
 print(f'5! = {fatorial(5)}')
-print(f'5! = {fatorial(7)}')
+print(f'7! = {fatorial(7)}')
 
 # n! = n * (n - 1)! p/ n > 1
-# Recursividade ocorre quando a definição de uma função inclui a própria 
-# Função sendo definida
+# Recursividade ocorre quando a definição de uma função inclui a própria
+# função sendo definida
 
 # Em programação, a recursividade se traduz quando uma função efetua
 # chamadas a si própria.
 
 # Implementação recursiva
 def fatorial2(n):
-    # Em uma função recursiva, a condição de saída é aquela em que 
-    # a função é capaz de retornar um resultado SEM chamar novamente 
+    # Em uma função recursiva, a condição de saída é aquela em que
+    # a função é capaz de retornar um resultado SEM chamar novamente
     # a si mesma
-    if n <= 1: # Condição de saída da função
+    if n <= 1:  # Condição de saída
         return 1
-    return n * fatorial(n - 1)
+    return n * fatorial2(n - 1)
 
-print('-------------------------------------------------------------')
-print(f'5! = {fatorial2(5)}') 
+print('---------------------------------------------------')
+print(f'519! = {fatorial2(519)}')
+
+#import sys
+#print(sys.maxsize)
