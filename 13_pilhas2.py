9c6f04b8bb47b48c0271a16070cb74c37ed7041e
matholv512 <matholv512@outlook.com>
2021-09-17 23:27:55 +0000

    (17/09) Criação da classe Stack

---
+++ 13_pilhas2.py
@@ -0,0 +1,29 @@
+from lib.stack import Stack
+
+# Criando uma pilha
+pilha = Stack()
+print(pilha.to_str())
+
+# Empilhando valores
+pilha.push(10)
+pilha.push(13)
+pilha.push(19)
+pilha.push(23)
+pilha.push(27)
+pilha.push(33)
+
+print(pilha.to_str())
+
+# Retirar um elemento da pilha
+removido = pilha.pop()
+print(f"Removido: {removido}, pilha {pilha.to_str()}")
+
+# Consultar o último elemento 
+ultimo = pilha.peek()
+print(f"Removido: {ultimo}, pilha {pilha.to_str()}")
+
+# Esvaziar a pilha, elemento por elemento
+while not pilha.is_empty():
+    print(pilha.pop())
+
+print(pilha.to_str())
